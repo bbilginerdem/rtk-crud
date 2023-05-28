ce9042c8be37789d11fc49f24a31113f455cdf9c
behzat bilgin erdem <bbilgin.erdem@gmail.com>
2023-05-28 11:20:05 +0300

    created file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css';
+import { Routes, BrowserRouter, Route } from "react-router-dom"
+import Home from './pages/Home';
+import AddEdit from './pages/AddEdit';
+import Info from './pages/Info';
 
 function App() {
   return (
     <div className="App">
-      <h2>RTK Query</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/addContact" element={<AddEdit />} />
+          <Route path="/editContact/:id" element={<AddEdit />} />
+          <Route path="/info/:id" element={<Info />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Info.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div><h2>Info</h2></div>
+  )
+}
+
+export default Info
